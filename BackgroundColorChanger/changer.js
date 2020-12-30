f520e2d0fb3389a729a8c3aa2cdb88997a37fbf9
aniler0 <anil.er@hotmail.com>
2020-12-30 14:42:36 +0200

    V1

---
+++ BackgroundColorChanger/changer.js
@@ -0,0 +1,16 @@
+function change() {
+    let random1 = Math.floor(Math.random() * 255);
+    let random2 = Math.floor(Math.random() * 255);
+    let random3 = Math.floor(Math.random() * 255);
+
+    document.body.style.backgroundColor = `rgb(${random1},${random2},${random3})`
+
+}
+
+
+
+
+
+
+
+
